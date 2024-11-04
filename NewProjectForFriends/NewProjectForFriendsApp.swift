5589bf6f2ecdc96af5610194811e47927c400483
aazzouz.fatima.ezzahra <aazzouz.fatima.ezzahra@gmail.com>
2024-11-04 16:37:45 +0100

    Initial Commit

---
+++ NewProjectForFriends/NewProjectForFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectForFriendsApp.swift
+//  NewProjectForFriends
+//
+//  Created by aazzouz fatima ezzahra on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectForFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
